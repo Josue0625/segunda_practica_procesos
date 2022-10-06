c6b3f234cda6f0fecb35fd398b7980f1cb2770ab
Josue0625 <jcampos@ufpso.edu.co>
2022-10-05 22:42:53 -0500

    Creacion del proyecto y cargue de las dependencias

---
+++ src/main/java/com/segunda_practica_procesos/josue/JosueApplication.java
@@ -0,0 +1,13 @@
+package com.segunda_practica_procesos.josue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JosueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JosueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/segunda_practica_procesos/josue/JosueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.segunda_practica_procesos.josue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JosueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
